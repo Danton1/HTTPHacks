8bf3664ba0ece3492e84d94a8edd7588b83c4a84
Danton Soares <dantonvsoares@gmail.com>
2025-11-08 19:01:59 -0800

    Adding settings files and options

---
+++ src/settings.h
@@ -0,0 +1,34 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+
+class Settings {
+public:
+    static std::string save_path;
+    static std::string voice_notes_path;
+    static std::string audio_input_device;
+    static bool always_on_top;
+    static bool hide_in_taskbar;
+    static std::string post_formatter;
+    static std::string keybinding_start_stop_recording;
+    static std::string keybinding_open_notes_window;
+
+    Settings();
+    void reset_settings();
+};
+
+class SettingsManager {
+public:
+    explicit SettingsManager(const std::string& path);
+    Settings getSettings() const;
+    bool applySettings();
+
+private:
+    std::string settingsPath;
+    Settings settings;
+
+    bool readSettings(std::string path);
+};
+
+#endif // SETTINGS_H
